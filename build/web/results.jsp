363338ff6e2fdffc71874e7f3bae2032af57785a
wupham <wupham@elon.edu>
2016-10-25 20:49:54 -0400

    first commit

---
+++ build/web/results.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : results
+    Created on : Oct 25, 2016, 2:51:17 PM
+    Author     : Wesley
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Value Calculator</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="homework2.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="main">
+            <h1>Value Calculator</h1>
+            <p>Investment Amount:   Result</p>
+            <p>Yearly Interest Rate:   Result</p>
+            <p>Number of Years:   Result</p>
+            <p>Future Value:   Result</p>
+        </div>
+        <div id="footer">
+            <p>&copy; 2016 Wes Upham and Sarah Hennenkamp</p>
+        </div>
+    </body>
+</html>
---
+++ src/java/edu/elon/calculate/CalculateServlet.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.elon.calculate;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Wesley
+ */
+public class CalculateServlet extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                String url = "/index.jsp";
+        
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.jsp";    // the "join" page
+        } 
+        else if (action.equals("add")) {
+            // get parameters from the request
+            String Amount = request.getParameter("Amount");
+            String Rate = request.getParameter("Rate");
+            String Years = request.getParameter("Years");
+            
+            // store data in User object
+
+
+            // validate the parameters
+            String message;
+            if (firstName == null || lastName == null || email == null ||
+                firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+                message = "Please fill out all three text boxes.";
+                url = "/index.jsp";
+            } 
+            else {
+                message = "";
+                url = "/result.jsp";
+ 
+            }
+            //request.setAttribute("user", user);
+            request.setAttribute("message", message);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+    }
