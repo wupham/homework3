1b093d2e57f3eccf251410ee4fe78eccaa2376a6
Nick Richu <nrichu@elon.edu>
2016-11-10 19:59:17 -0500

    minor changes

--- src/java/tags/currencyFormat.java
+++ src/java/tags/currencyFormat.java
@@ -3,18 +3,20 @@ package tags;
 import javax.servlet.jsp.*;
 import javax.servlet.jsp.tagext.*;
 import java.io.*;
-
+import edu.elon.calculate.CalculateServlet;
 import java.text.NumberFormat;
 import static javax.servlet.jsp.tagext.Tag.SKIP_BODY;
-import edu.elon.calculate;
+
 
 public class currencyFormat extends TagSupport {
-
+CalculateServlet class1 = new CalculateServlet();
     @Override
     public int doStartTag() throws JspException {
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
-      String resultAmount = formatter.format(amountd);
-	String resultRate = Double.toString(rated);
+        //get this shit to work. i want to access the variables in class1 which is an object of CalculateServlet
+        //were gucci if we can fgure this out, everything else will be smooth
+       String resultAmount = formatter.format(class1.getClass().getField(amountd));
+	String resultRate = Double.toString(class1.getClass().getField(rated));
 	String resultValue = formatter.format(value);
 	
 
